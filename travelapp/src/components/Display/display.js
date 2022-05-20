71095df1c736c3d1dd4b059741ef9ebe93aa52da
NicholasGomis <nicholasgomis@icloud.com>
2022-05-20 13:38:03 +0100

    Added info boxes

---
+++ travelapp/src/components/Display/display.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Info } from '../Info/info.js'
+
+export function Display(props) {
+    
+
+    return (
+     
+        <div>
+            {/* <Info currency={}/> */}
+        </div>
+
+    )
+}
