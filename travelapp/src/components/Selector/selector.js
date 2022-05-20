974f6e13817923d702cfdbaa985fb0aed5dd5f10
nicfheorais <7238289+nicfheorais@users.noreply.github.com>
2022-05-20 14:23:55 +0100

    selector componenets all printing.  Find Holiday button retrieving the countries, and the handleclick is resetting the button text

--- travelapp/src/components/Selector/selector.js
+++ travelapp/src/components/Selector/selector.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import FindHoliday from "../FindHoliday/findHoliday.js";
+import Header from "../Header/header.js";
 
-export function Index(props) {
-    
+export function Selector({ buttonText, handleClick }) {
+    const selectorDescription =
+        "Everyone is ready to travel again - Don't know where to go? we are here to help. Let us randomly choose your next holiday destination for you!";
+
+    const selectorBackgroundImage = ""; //TODO: replace with actual image path
 
     return (
-        <>
-            
-        </>
-    )
+        <div className="selector">
+            <Header />
+            <p className="selector-description"> {selectorDescription}</p>
+            {/* need to render background image here  */}
+            <FindHoliday buttonText={buttonText} handleClick={handleClick} />
+        </div>
+    );
 }
+
+export default Selector;
