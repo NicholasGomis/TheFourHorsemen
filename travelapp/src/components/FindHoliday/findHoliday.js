b039b61454479dc61b4b44ff9c871a1ea31585bb
nicfheorais <7238289+nicfheorais@users.noreply.github.com>
2022-05-23 10:12:34 +0100

    Added basic button css, a footer placeholder, and changed Header title text

--- travelapp/src/components/FindHoliday/findHoliday.js
+++ travelapp/src/components/FindHoliday/findHoliday.js
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import "./findHoliday.css";
 
-function FindHoliday({buttonText, handleClick}) {
+function FindHoliday({ buttonText, handleClick }) {
     return (
         <div className="find-holiday-button">
             <button onClick={handleClick}>{buttonText}</button>
